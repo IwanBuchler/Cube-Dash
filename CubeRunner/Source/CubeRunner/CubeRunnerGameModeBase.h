7720cb07088f36e60a472bf1765ad0f841067183
Iwan <iwanbuchler02@gmail.com>
2020-10-09 09:49:02 +0200

    C++ Project made in Unreal Engine 4.24
    -Character movement constructed with C++
    -Locationing and object setup done in C++
    -Character death and spawn logic in C++
    -HUD logic made with Blueprints

---
+++ CubeRunner/Source/CubeRunner/CubeRunnerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CubeRunnerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUBERUNNER_API ACubeRunnerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
